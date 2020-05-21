77698c4007593dfbfbedd6f42ab5aba43668b221
Antknee <anthonycolindres@yahoo.com>
2020-05-20 20:51:24 -0400

    Updates

---
+++ precache-manifest.b2ffb95d135a1e7fe844ca6ed7084c91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db7f5b37a58054c333d8cafdce6d864e",
+    "url": "/reactEmpDir/index.html"
+  },
+  {
+    "revision": "92115f813dfdfa29a14f",
+    "url": "/reactEmpDir/static/css/main.d6b0dc45.chunk.css"
+  },
+  {
+    "revision": "7075e947978bf1501917",
+    "url": "/reactEmpDir/static/js/2.8cb27924.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactEmpDir/static/js/2.8cb27924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92115f813dfdfa29a14f",
+    "url": "/reactEmpDir/static/js/main.4854cf6f.chunk.js"
+  },
+  {
+    "revision": "8c544c7ae801cd5a8f2b",
+    "url": "/reactEmpDir/static/js/runtime-main.1fb0dc0a.js"
+  }
+]);
